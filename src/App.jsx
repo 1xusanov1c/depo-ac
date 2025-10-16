aef393740a0157fd6fbf0a677ca73f5632a91f6d
1xusanov1c <azz806@gmail.com>
2025-10-16 18:28:04 +0500

    first push

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Card from "./components/card"
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className='text-red-600 bg-red-400 p-3 rounded-xl'>
+        <Card/>
+      </h1>
+    </>
+  )
+}
+
+export default App
